cd7b718d4d62a2591a357a422ac36528eaa38747
Gustavo Todeschini <gutodeschinii@outlook.com>
2025-03-13 11:29:37 -0300

    Fazendo com que funcionasse os botões de excluir e começo de configurações para Adicionar tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -22,15 +22,32 @@ function App() {
     isCompleted: false  
   },
 
-])
+]);
+
+function onTaskClick(taskId) {
+  const newTasks = tasks.map(task => {
+    // PRECISO ATUALIZAR ESSA TAREFA
+    if (task.id === taskId) {
+      return {...task, isCompleted: !task.isCompleted}
+    } 
+
+    // NÃO PRECISO ATUALIZAR ESSA TAREFA
+    return task;
+  });
+  setTasks(newTasks);
+}
+
+function removeTask(taskId) {
+  setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));  
+}
 
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1> 
         <AddTask />
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} removeTask={removeTask}/>
       </div>
     </div>
   )
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,11 @@
 function AddTask() {
-    <h1>Add Task</h1>
+   return (
+   <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+        <input type="text" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"  placeholder="Digite o título da tarefa"/>
+        <input type="text" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"  placeholder="Digite a descrição da tarefa"/>
+        <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
+    </div>
+    );
 }
 
 export default AddTask
