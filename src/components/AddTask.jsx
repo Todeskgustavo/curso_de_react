6733ad948222b13d97775f4f379b1e46dc487a39
Gustavo Todeschini <gutodeschinii@outlook.com>
2025-03-13 13:44:30 -0300

    Fazendo a sessão ver detalhes

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,32 @@
-function AddTask() {
-   return (
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+   const[title, setTitle] = useState("");
+   const[description, setDescription] = useState("");
+   
+    return (
    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
-        <input type="text" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"  placeholder="Digite o título da tarefa"/>
-        <input type="text" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"  placeholder="Digite a descrição da tarefa"/>
-        <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
+        <input type="text" 
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"  
+        placeholder="Digite o título da tarefa"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        />
+        <input type="text" 
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"  
+        placeholder="Digite a descrição da tarefa"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+        />
+        <button onClick={() => {
+            // Verificar se o título e a descrição estão preenchidos
+            if (!title.trim() || !description.trim()) {
+                return alert("Preencha o título e a descrição da tarefa.");
+            }
+            onAddTaskSubmit(title, description);
+            setTitle("");
+            setDescription("");
+        }} className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
     </div>
     );
 }
